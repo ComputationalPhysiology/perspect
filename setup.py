4955ff4b206845978902ec67fe8b508bd687a82d
Alexandra Diem <alexandra.diem@gmail.com>
2019-09-03 10:47:53 +0200

    inital setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='perspect',
+      version='0.1',
+      description='Perfusion SPECT model',
+      url='https://github.com/ComputationalPhysiology/perspect',
+      author='Alexandra K. Diem',
+      author_email='alexandra.diem@gmail.com',
+      license='BSD3',
+      packages=['perspect'],
+      zip_safe=False)
