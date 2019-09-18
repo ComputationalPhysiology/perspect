6acf202a8a1cb906a2104723f06bcd7e4dff30cf
Alexandra Diem <alexandra.diem@gmail.com>
2019-09-18 11:31:48 +0200

    added test for update_displacement()

--- tests/test_porousproblem.py
+++ tests/test_porousproblem.py
@@ -1,4 +1,5 @@
 import perspect
+import pulse
 import dolfin as df
 import pytest
 
@@ -34,10 +35,30 @@ def test_init_porous_form(porous_problem):
     assert sum(df.assemble(p._form)[:]) == 0
 
 
+def test_update_mechanics(porous_problem, mechanics_problem):
+    mechanics_problem.solve()
+    mu, mp = mechanics_problem.state.split(deepcopy=True)
+    assert(df.errornorm(porous_problem.displacement, mu) < 1e-5)
+
+
 @pytest.fixture
 def geometry():
     geometry = perspect.HeartGeometry.from_file(perspect.mesh_paths["simple_ellipsoid"])
     return geometry
+
+@pytest.fixture
+def material(geometry):
+    activation = df.Function(df.FunctionSpace(geometry.mesh, "R", 0))
+    activation.assign(df.Constant(0.0))
+    matparams = pulse.HolzapfelOgden.default_parameters()
+    material = pulse.HolzapfelOgden(activation=activation,
+                                    parameters=matparams,
+                                    active_model="active_stress",
+                                    eta=0.3,
+                                    f0=geometry.f0,
+                                    s0=geometry.s0,
+                                    n0=geometry.n0)
+    return material
 
 @pytest.fixture
 def bcs():
@@ -53,3 +74,9 @@ def parameters():
 def porous_problem(geometry, bcs, parameters):
     porous_problem = perspect.PorousProblem(geometry, bcs=bcs, parameters=parameters)
     return porous_problem
+
+@pytest.fixture
+def mechanics_problem(geometry, material, bcs):
+    mechanics_problem = pulse.MechanicsProblem(geometry, material, bcs=bcs,
+                                            bcs_parameters={"": ""})
+    return mechanics_problem
