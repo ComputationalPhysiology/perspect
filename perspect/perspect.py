67bd9b3165903d46db2e9f9b0b4694f93da18c31
Alexandra Diem <alexandra.diem@gmail.com>
2019-09-23 15:00:50 +0200

    Made geometry and material visible at top level Perspect object

--- perspect/perspect.py
+++ perspect/perspect.py
@@ -10,6 +10,8 @@ class Perspect(object):
 
     def __init__(self, geometry, material, mechanics_bcs=None, porous_bcs=None,
                     parameters=None):
+        self.geometry = geometry
+        self.material = material
         self.pprob = PorousProblem(geometry, material, bcs=porous_bcs, parameters=parameters)
         self.mprob = pulse.MechanicsProblem(geometry, material, bcs=mechanics_bcs,
                                             bcs_parameters={"": ""})
@@ -24,6 +26,10 @@ class Perspect(object):
         mu, mp = self.mprob.state.split(deepcopy=True)
         self.update_mechanics(mu)
         self.solve_porous()
+
+
+    def iterate(self, control, target, **kwargs):
+        pulse.iterate.iterate(self.mprob, control, target, **kwargs)
 
 
     def solve_mechanics(self):
